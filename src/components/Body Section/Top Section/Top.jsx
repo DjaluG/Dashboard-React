42390fa8e45a0b962dcedfc5ef7388667ab3b151
TezukaKarlsefni <DjaluKun@gmail.com>
2023-03-11 07:14:21 +0700

    first

--- src/components/Body Section/Top Section/Top.jsx
+++ src/components/Body Section/Top Section/Top.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './top.css'
 
+import {BiSearchAlt} from 'react-icons/bi'
+
 const Top = () => {
   return (
-    <div>Top</div>
+    <div className='topSection'>
+      <div className="headerSection flex">
+        <div className="title">
+          <h1>Welcom to Planti</h1>
+          <p>Hello Dummy, Welcome Bakc!</p>
+        </div>
+      </div>
+
+      <div className="searchBar flex">
+        <input type="text" placeholder='Search Here'/>
+        <BiSearchAlt className='icon'/>
+      </div>
+    </div>
   )
 }
 
